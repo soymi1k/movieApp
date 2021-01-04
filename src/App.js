24dabb9286ece0590db280a469b58e2c2253bd09
soymi1k <soymilkneigh@gmail.com>
2021-01-04 22:47:13 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,31 +2,17 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-  add = () => { //인자값을 갖지않는 add라는 이름의 function 생성
-    this.setState(current => ({ count : current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count : current.count - 1 }));
+    isLoading: true,
+    movies: []
   };
   componentDidMount() {
-    console.log("Component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
